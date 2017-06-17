3e2c74a921de6f45d09fa8782c238f43d8872fb3
hicks27 <hicksp2597@student.faytechcc.edu>
2017-06-17 15:54:31 -0700

    Add files via upload
    "M3T1 completed"

---
+++ M3T1_AreasOfRectangles_PatriciaHicks.py
@@ -0,0 +1,10 @@
+Python 3.6.1 (v3.6.1:69c0db5, Mar 21 2017, 17:54:52) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+============ RESTART: G:/M3T1_AreasOfRectangles_PatriciaHicks.py ============
+Enter length of rectangle 1:10
+ Enter width of retangle 1:6
+Enter length of rectangle 2:18
+Enter width of rectangle 2:9
+Rectangle 2 has the greater area.
+>>> 
